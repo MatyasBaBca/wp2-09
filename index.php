4a1bceecaa3011036d05151de699d108fb5538f1
MatyasBaBca <47145006+MatyasBaBca@users.noreply.github.com>
2019-11-07 13:02:24 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$projects=array(
+"wp2-01",
+"wp2-02",
+"wp2-03",
+"wp2-04",
+"wp2-05",
+);
+echo count($projects);
+$count=count($projects);
+$width = 20*$count;
+echo $width
+?>
+
+<div style="height: 200px; width: 200px; background-color: blue; display: block;"> 
+<span style="height: 200px; width: 100px; background-color: red; display: block;"> </span>
+</div>
